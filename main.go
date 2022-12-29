aceff1deaa9c06c82363a1cc28fdb69eb34f4a1e
stupidfish <2928109164@qq.com>
2022-12-29 15:57:03 +0800

    1

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 				ret := goreq.Do(req)
 				if strings.Contains(ret.Text, host) {
 					println(proxyStr)
-					file, err := os.OpenFile("output.txt", os.O_APPEND|os.O_CREATE, 0666)
+					file, err := os.OpenFile("output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModeAppend)
 					if err != nil {
 						return err
 					}
