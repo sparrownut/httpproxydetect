4b2a1180c9df285f1edece4d12d2b06b137ac891
stupidfish <2928109164@qq.com>
2022-12-29 15:22:32 +0800

    1

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/urfave/cli/v2"
 	"github.com/zhshch2002/goreq"
 	"os"
 	"strings"
@@ -8,19 +10,43 @@ import (
 )
 
 func main() {
+	port := "8080"
+	app := &cli.App{
+		Name:      "proxydetect",
+		Usage:     "judg proxy can use",
+		UsageText: "lazy to write...",
+		Version:   "0.4.4",
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "port", Aliases: []string{"p"}, Destination: &port, Value: "8080", Usage: "port"},
+		},
+		HideHelpCommand: true,
+		Action: func(c *cli.Context) error {
+		start:
+			host := ""
+			_, err := fmt.Scanln(&host)
+			if err != nil {
+				return err
+			}
+			proxyStr := "http://" + host + ":" + port
+			client := goreq.NewClient()
+			req := goreq.Get("http://icanhazip.com/").SetClient(client).SetProxy(proxyStr).SetTimeout(5 * time.Second)
+			//fmt.Printf(proxyStr)
+			if req.Err == nil {
+				ret := goreq.Do(req)
+				if strings.Contains(ret.Text, host) {
+					println(proxyStr)
+				}
+			}
+
+			goto start
+			//return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		panic(err)
+	}
+
 	//fmt.Printf(os.Args[1])
-	proxyStr := "http://" + os.Args[1] + "8080"
-	client := goreq.NewClient()
-	req := goreq.Get("https://www.baidu.com").SetClient(client).SetProxy(proxyStr).SetTimeout(5 * time.Second)
-	if req.Err != nil {
-		return
-	}
-	ret := goreq.Do(req)
-	if ret.Err != nil {
-		return
-	}
-	//fmt.Printf(ret.Text)
-	if strings.Contains(ret.Text, "www.baidu.com") {
-		println(proxyStr)
-	}
+
 }
