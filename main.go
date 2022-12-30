7fe6aefd4743a374dbcf3abed19f12cf8724bea1
stupidfish <2928109164@qq.com>
2022-12-30 15:52:10 +0800

    更名为protocoldetect 增加ssh协议

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/urfave/cli/v2"
 	"github.com/zhshch2002/goreq"
+	"net"
 	"os"
 	"strings"
 	"time"
@@ -11,51 +12,23 @@ import (
 
 func main() {
 	port := "8080"
+	protocol := "http"
 	app := &cli.App{
-		Name:      "proxydetect",
-		Usage:     "judg proxy can use",
+		Name:      "protocaldetect",
+		Usage:     "judg protocal can use protocol:\nhttp\nssh", // 这里写协议
 		UsageText: "lazy to write...",
 		Version:   "0.4.4",
 		Flags: []cli.Flag{
-			&cli.StringFlag{Name: "port", Aliases: []string{"p"}, Destination: &port, Value: "8080", Usage: "port"},
+			&cli.StringFlag{Name: "port", Aliases: []string{"p"}, Destination: &port, Value: "8080", Usage: "port", Required: true},
+			&cli.StringFlag{Name: "protocol", Aliases: []string{"P"}, Destination: &protocol, Value: "8080", Usage: "protocol", Required: true},
 		},
 		HideHelpCommand: true,
 		Action: func(c *cli.Context) error {
-			file, fileerrerr := os.OpenFile("output.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-			if fileerrerr != nil {
-				return fileerrerr
-			}
-			defer func(file *os.File) {
-				err := file.Close()
-				if err != nil {
-
-				}
-			}(file)
-		start:
-			host := ""
-			_, err := fmt.Scanln(&host)
+			err := do(port, protocol)
 			if err != nil {
 				return err
 			}
-			proxyStr := "http://" + host + ":" + port
-			client := goreq.NewClient()
-			req := goreq.Get("http://icanhazip.com/").SetClient(client).SetProxy(proxyStr).SetTimeout(5 * time.Second)
-			//fmt.Printf(proxyStr)
-			if req.Err == nil {
-				ret := goreq.Do(req)
-				if strings.Contains(ret.Text, host) {
-					println(proxyStr)
-
-					_, writeerr := file.WriteString(proxyStr + "\n")
-					if writeerr != nil {
-						return writeerr
-					}
-
-				}
-			}
-
-			goto start
-			//return nil
+			return nil
 		},
 	}
 	err := app.Run(os.Args)
@@ -65,4 +38,85 @@ func main() {
 
 	//fmt.Printf(os.Args[1])
 
+}
+func do(port string, protocol string) error {
+	file, fileerrerr := os.OpenFile("output.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if fileerrerr != nil {
+		return fileerrerr
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+
+		}
+	}(file)
+	// 文件初始化
+start: // 在这里循环
+	host := ""
+	_, err := fmt.Scanln(&host)
+	if err != nil {
+		return err
+	}
+	go func() {
+		err := dofunc(port, protocol, file, host)
+		if err != nil {
+
+		}
+	}()
+	goto start
+}
+func dofunc(port string, protocol string, file *os.File, host string) error {
+	if protocol == "http" {
+
+		proxyStr := "http://" + host + ":" + port
+		client := goreq.NewClient()
+		req := goreq.Get("http://icanhazip.com/").SetClient(client).SetProxy(proxyStr).SetTimeout(5 * time.Second)
+		//fmt.Printf(proxyStr)
+		if req.Err == nil {
+			ret := goreq.Do(req)
+			if strings.Contains(ret.Text, host) {
+				println(proxyStr) // 输出
+				_, writeerr := file.WriteString(proxyStr + "\n")
+				if writeerr != nil {
+					return writeerr
+				}
+
+			}
+		}
+
+		//return nil
+	} else if protocol == "ssh" {
+		file, fileerrerr := os.OpenFile("output.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+		if fileerrerr != nil {
+			return fileerrerr
+		}
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+
+			}
+		}(file)
+		dial, err := net.Dial("tcp", host+":"+port)
+		if err != nil {
+			return err
+		}
+		_, err = dial.Write([]byte("")) // 发送空消息
+		if err != nil {
+			return err
+		}
+		buf := [512]byte{}
+		n, err := dial.Read(buf[:])
+		//println(string(buf[:n]))
+		if strings.Contains(string(buf[:n]), "SSH") {
+			println(host)
+			_, writeerr := file.WriteString("[SSH]" + host + ":" + port + "\n")
+			if writeerr != nil {
+				return writeerr
+			}
+		}
+	} else {
+		fmt.Printf("无此协议")
+		return nil
+	}
+	return nil
 }
